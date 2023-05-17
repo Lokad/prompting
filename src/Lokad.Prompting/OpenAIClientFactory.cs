8a144d4733359d5c596c1fb7c97df92faaf2e288
Eslam Hamouda <eslamx7@gmail.com>
2023-05-17 14:55:45 +0300

    add OpenAIClientFactory class

---
+++ src/Lokad.Prompting/OpenAIClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lokad.Prompting;
+
+public class OpenAIClientFactory : IHttpClientFactory
+{
+    public HttpClient CreateClient(string name)
+    {
+        return new HttpClient
+        {
+            // The OpenAI API does usually answers albeit with considerable delay.
+            Timeout = TimeSpan.FromMinutes(5)
+        };
+    }
+}
