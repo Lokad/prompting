23a8f2d2eff67494ac1ad501eb601f02a9031d99
Joannes Vermorel <joannes.vermorel@lokad.com>
2023-06-02 15:09:22 +0200

    retry policy on 429 Too Many Requests

--- src/Lokad.Prompting/OpenAIClientFactory.cs
+++ src/Lokad.Prompting/OpenAIClientFactory.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Lokad.Prompting;
+﻿namespace Lokad.Prompting;
 
 public class OpenAIClientFactory : IHttpClientFactory
 {
     public HttpClient CreateClient(string name)
     {
-        return new HttpClient
+        return new HttpClient(new RetryHandler())
         {
             // The OpenAI API does usually answers albeit with considerable delay.
             Timeout = TimeSpan.FromMinutes(5)
         };
+    }
+
+    public class RetryHandler : DelegatingHandler
+    {
+        private const int maxRetries = 5;
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var delay = TimeSpan.FromSeconds(1); // Initial delay
+            for (int i = 0; i < maxRetries; i++)
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+
+                // 429 Too Many Requests
+                if (response.StatusCode == (System.Net.HttpStatusCode)429 && i < maxRetries)
+                {
+                    await Task.Delay(delay, cancellationToken);
+                    delay *= 2; // Exponential backoff
+                    continue;
+                }
+
+                return response;
+            }
+
+            throw new HttpRequestException($"Retry attempts failed after {maxRetries} retries.");
+        }
     }
 }
